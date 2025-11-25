0bba7d79b6e2791db0b8c54ead0d86066cfaae95
senyaevil <60550469+senyaevil@users.noreply.github.com>
2025-11-25 13:23:13 +0300

    Add files via upload

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,14 +3,14 @@ import runEngine from '../index.js'
 
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-  case '+':
-    return num1 + num2
-  case '-':
-    return num1 - num2
-  case '*':
-    return num1 * num2
-  default:
-    throw new Error(`Unknown operator: ${operator}`)
+    case '+':
+      return num1 + num2
+    case '-':
+      return num1 - num2
+    case '*':
+      return num1 * num2
+    default:
+      throw new Error(`Unknown operator: ${operator}`)
   }
 }
 
