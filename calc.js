f1701a954e4e2220aa1dab8caa0a3b9bdcaa3bee
senyaevil <60550469+senyaevil@users.noreply.github.com>
2025-11-25 13:22:20 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,34 @@
+import getRandomNumber from '../utils.js'
+import runEngine from '../index.js'
+
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2
+    case '-':
+      return num1 - num2
+    case '*':
+      return num1 * num2
+    default:
+      throw new Error(`Unknown operator: ${operator}`)
+  }
+}
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 25)
+  const num2 = getRandomNumber(1, 25)
+  const operators = ['+', '-', '*']
+  const operator = operators[getRandomNumber(0, operators.length - 1)]
+
+  const question = `${num1} ${operator} ${num2}`
+  const correctAnswer = String(calculate(num1, num2, operator))
+
+  return [question, correctAnswer]
+}
+
+const runCalcGame = () => {
+  const rules = 'What is the result of the expression?'
+  runEngine(rules, generateRound)
+}
+
+export default runCalcGame
