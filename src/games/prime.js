be16e73f22361aab8a3339a8ca0ad1ee6c994c75
senyaevil <60550469+senyaevil@users.noreply.github.com>
2025-11-25 12:43:59 +0300

    Add files via upload

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,38 +2,38 @@ import getRandomNumber from '../utils.js'
 import runEngine from '../index.js'
 
 const isPrime = (number) => {
-  if (number < 2) {
-    return false
-  }
-
-  if (number === 2) {
-    return true
-  }
-
-  if (number % 2 === 0) {
-    return false
-  }
-
-  const limit = Math.sqrt(number)
-  for (let i = 3; i <= limit; i += 2) {
-    if (number % i === 0) {
-      return false
+    if (number < 2) {
+        return false
     }
-  }
 
-  return true
+    if (number === 2) {
+        return true
+    }
+
+    if (number % 2 === 0) {
+        return false
+    }
+
+    const limit = Math.sqrt(number)
+    for (let i = 3; i <= limit; i += 2) {
+        if (number % i === 0) {
+            return false
+        }
+    }
+
+    return true
 }
 
 const generateRound = () => {
-  const question = getRandomNumber(1, 100)
-  const correctAnswer = isPrime(question) ? 'yes' : 'no'
+    const question = getRandomNumber(1, 100)
+    const correctAnswer = isPrime(question) ? 'yes' : 'no'
 
-  return [question, correctAnswer]
+    return [question, correctAnswer]
 }
 
 const runPrimeGame = () => {
-  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-  runEngine(rules, generateRound)
+    const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    runEngine(rules, generateRound)
 }
 
 export default runPrimeGame
--- src/games/progression.js
+++ src/games/progression.js
@@ -2,31 +2,31 @@ import getRandomNumber from '../utils.js'
 import runEngine from '../index.js'
 
 const generateProgression = (start, step, length) => {
-  const progression = []
-  for (let i = 0; i < length; i += 1) {
-    progression.push(start + i * step)
-  }
-  return progression
+    const progression = []
+    for (let i = 0; i < length; i += 1) {
+        progression.push(start + i * step)
+    }
+    return progression
 }
 
 const generateRound = () => {
-  const progressionLength = getRandomNumber(5, 10)
-  const start = getRandomNumber(1, 20)
-  const step = getRandomNumber(1, 10)
-  const hiddenIndex = getRandomNumber(0, progressionLength - 1)
+    const progressionLength = getRandomNumber(5, 10)
+    const start = getRandomNumber(1, 20)
+    const step = getRandomNumber(1, 10)
+    const hiddenIndex = getRandomNumber(0, progressionLength - 1)
 
-  const progression = generateProgression(start, step, progressionLength)
-  const correctAnswer = String(progression[hiddenIndex])
+    const progression = generateProgression(start, step, progressionLength)
+    const correctAnswer = String(progression[hiddenIndex])
 
-  progression[hiddenIndex] = '..'
-  const question = progression.join(' ')
+    progression[hiddenIndex] = '..'
+    const question = progression.join(' ')
 
-  return [question, correctAnswer]
+    return [question, correctAnswer]
 }
 
 const runProgressionGame = () => {
-  const rules = 'What number is missing in the progression?'
-  runEngine(rules, generateRound)
+    const rules = 'What number is missing in the progression?'
+    runEngine(rules, generateRound)
 }
 
 export default runProgressionGame
